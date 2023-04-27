dabdb4d097eb63ab6c5e2e37f26dd3507bd85fd7
JorgeRuiz-Hexaware <112002374+JorgeRuiz-Hexaware@users.noreply.github.com>
2023-04-27 15:41:15 -0600

    Update OpenApp.java

--- OpenApp.java
+++ OpenApp.java
@@ -2,9 +2,10 @@ import java.io.*;
 import java.lang.*;
 
 public class OpenApp {
+  
+  String [] a = new String[]{"open","/Applications/Chess.app"};
   public static void main(String[] args) {
     System.out.println("Opening App");
-    String [] a = new String[]{"open","/Applications/Chess.app"};
     Runtime.exec(a);
   }
 }
